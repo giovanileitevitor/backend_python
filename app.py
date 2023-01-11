dc8810a4181dab533d8703e5f172abd78a18373b
Giovani Vitor <giovanileitevitor@gmail.com>
2023-01-11 09:14:46 -0300

    added first python file

---
+++ app.py
@@ -0,0 +1,14 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello():
+#     return "Hello World!"
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+a = 3
+b = 4
+print(a+b)
